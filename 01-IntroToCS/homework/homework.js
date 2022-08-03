c3035e1b1f9115b30878436ac72077f74e14477e
JerryMurillo <jealmuco@gmail.com>
2022-08-03 17:31:11 -0500

    Lession 1 Completada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,26 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  var decimal = 0, i = 0, resto;
+    
+  while (num != 0) {
+    resto = num % 10;
+    num = Number.parseInt(num / 10);
+    decimal = decimal + resto * Math.pow(2, i);
+    ++i;
+  }
+  return decimal;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  let binario = ''
+  while (num > 1) {
+    let residuo = num % 2
+    binario = `${residuo}${binario}`
+    num = (num - residuo) / 2
+  }
+  binario = `${num}${binario}`
+  return binario
 }
 
 
